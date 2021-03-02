7d9a35ea4991693aa13a39ce141448b6ad346786
Shang Shi <stoneshishang@gmail.com>
2021-03-02 15:03:32 -0500

    get the enzyme working

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
 });
